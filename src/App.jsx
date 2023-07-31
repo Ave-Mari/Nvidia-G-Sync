b8e0b6d95dec83ed12b1cb1a38fdda40eade8fe1
Mari Vaypane <avemari98@gmail.com>
2023-08-01 02:44:11 +0300

    create basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+//components
+import Header from './components/Header.jsx'
+import Description from './components/Description.jsx'
+import Technologies from './components/Technologies.jsx'
 
 function App() {
 
   return (
     <main>
-      
+      <Header />
+      <Description />
+      <Technologies />      
     </main>
   )
 }

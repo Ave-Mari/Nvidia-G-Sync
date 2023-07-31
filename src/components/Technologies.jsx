e79af410f78e75da5bd4a4a26a5c62b1d63e897b
Mari Vaypane <avemari98@gmail.com>
2023-08-01 02:55:07 +0300

    add data and map it to page

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React from 'react';
+
+const technologiesData = [
+    {
+        img: '',
+        headline: '',
+        description: ''
+    },
+    {
+        img: '',
+        headline: '',
+        description: ''
+    },
+    {
+        img: '',
+        headline: '',
+        description: ''
+    }
+]
 
 export default function Technologies() {
   return (
-    <div>Technologies</div>
+    <section>
+        <ul>
+            {technologiesData.map((item) => {
+                return (
+                    <li>
+                        <img src={item.img} alt={item.headline} />
+                        <h2>{item.headline}</h2>
+                        <p>{item.description}</p>
+                    </li>
+                )
+            })}
+        </ul>
+        <button>КУПИТЬ</button>
+    </section>
   )
 }

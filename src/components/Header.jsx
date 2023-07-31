57e5915453905a4d33e64d2ec4e5b84d47f95c1d
Mari Vaypane <avemari98@gmail.com>
2023-08-01 02:55:52 +0300

    add logo, menu and social links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,25 @@ import React from 'react'
 export default function Header() {
   return (
     <header>
-        <div className="div">
-            
+        <div className="menu">
+            <img></img>
+            <nav>
+                <ul>
+                    <li>ПРОДУКТЫ</li>
+                    <li>ТЕХНОЛОГИИ</li>
+                    <li>EXPERIENCE</li>
+                </ul>
+            </nav>
+
+
+        </div>
+        <div className="social">
+            <a href='' target='_blanc'>
+
+            </a>
+            <a href='' target='_blanc'>
+
+            </a>
         </div>
 
     </header>

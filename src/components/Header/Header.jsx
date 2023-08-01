8b8045d0229194546eee3e786646c71d3243fbe5
Mari Vaypane <avemari98@gmail.com>
2023-08-02 01:47:28 +0300

    add icon to links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 //img
 import logo from './logo.svg';
+import twitter from './tw.svg';
+import instagram from './inst.svg';
 
 export default function Header() {
   return (
     <header>
         <div className="menu">
+            <div className="logo-block">
             <img src={logo} alt='nvidia logo'></img>
+            </div>           
             <nav>
                 <ul>
                     <li>
@@ -25,10 +29,10 @@ export default function Header() {
         </div>
         <div className="social">
             <a href='' target='_blanc'>
-
+                <img src={twitter} alt='twitter' />
             </a>
             <a href='' target='_blanc'>
-
+                <img src={instagram} alt='instagram' />
             </a>
         </div>
 

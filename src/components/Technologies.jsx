a4888a1d4e5727ee07be44cfe4d9803e2f43a357
Mari Vaypane <avemari98@gmail.com>
2023-08-01 03:05:10 +0300

    add data to array

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -3,18 +3,27 @@ import React from 'react';
 const technologiesData = [
     {
         img: '',
-        headline: '',
-        description: ''
+        headlineBold: 'HDR ',
+        headline: 'DISPLAY',
+        description: 'Получите более широкий и комфортный для восприятия диапазон цветовой гаммы, чем в обычных мониторах.'
     },
     {
         img: '',
-        headline: '',
-        description: ''
+        headlineBold: 'INPUT ',
+        headline: 'LAG',
+        description: 'Мониторы с технологией G-SYNC обеспечивают минимальную задержку между нажатием клавиш и результатом на экране, что очень важно для геймеров.'
     },
     {
         img: '',
-        headline: '',
-        description: ''
+        headlineBold: 'HIGH REFRESH ',
+        headline: 'RATE',
+        description: 'NVIDIA тесно сотрудничает с производителями мониторов, чтобы вывести на рынок широкий спектр дисплеев с высокой частотой обновления экрана от 75 до 360 Гц.'
+    },
+    {
+        img: '',
+        headlineBold: 'WIDE COLOR ',
+        headline: 'GAMUT',
+        description: 'Современные мониторы G-SYNC ULTIMATE поддерживают цветовой охват DCI-P3, обеспечивая более реалистичную цветопередачу с плавными переходами.'
     }
 ]
 
@@ -26,7 +35,10 @@ export default function Technologies() {
                 return (
                     <li>
                         <img src={item.img} alt={item.headline} />
-                        <h2>{item.headline}</h2>
+                        <h2>
+                            <b>{item.headlineBold}</b>
+                            {item.headline}
+                        </h2>
                         <p>{item.description}</p>
                     </li>
                 )

906e44e7f04cdad3ad93d1af88f5f99d59be6017
Mari Vaypane <avemari98@gmail.com>
2023-08-02 14:24:12 +0300

    add images to component

--- src/components/Technologies/Technologies.jsx
+++ src/components/Technologies/Technologies.jsx
@@ -1,28 +1,33 @@
 import React from 'react';
 //styles
-import './Technologies'
+import './Technologies.scss'
+//images
+import normal from './normal.svg'
+import clock from './clock.svg'
+import power from './power.svg'
+import color from './color.svg'
 
 const technologiesData = [
     {
-        img: '',
+        img: normal,
         headlineBold: 'HDR ',
         headline: 'DISPLAY',
         description: 'Получите более широкий и комфортный для восприятия диапазон цветовой гаммы, чем в обычных мониторах.'
     },
     {
-        img: '',
+        img: clock,
         headlineBold: 'INPUT ',
         headline: 'LAG',
         description: 'Мониторы с технологией G-SYNC обеспечивают минимальную задержку между нажатием клавиш и результатом на экране, что очень важно для геймеров.'
     },
     {
-        img: '',
+        img: power,
         headlineBold: 'HIGH REFRESH ',
         headline: 'RATE',
         description: 'NVIDIA тесно сотрудничает с производителями мониторов, чтобы вывести на рынок широкий спектр дисплеев с высокой частотой обновления экрана от 75 до 360 Гц.'
     },
     {
-        img: '',
+        img: color,
         headlineBold: 'WIDE COLOR ',
         headline: 'GAMUT',
         description: 'Современные мониторы G-SYNC ULTIMATE поддерживают цветовой охват DCI-P3, обеспечивая более реалистичную цветопередачу с плавными переходами.'

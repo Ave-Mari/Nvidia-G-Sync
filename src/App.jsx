dd137db7708c8d08ff94bb3dda59662bb9481294
Mari Vaypane <avemari98@gmail.com>
2023-08-03 02:47:21 +0300

    move burger menu to separate component

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Header from './components/Header/Header.jsx'
 import Description from './components/Description/Description.jsx'
 import Technologies from './components/Technologies/Technologies.jsx'
 import Popup from './components/Popup/Popup.jsx'
+import BurgerMenu from './components/BurgerMenu/BurgerMenu.jsx'
 
 function App() {
 
   const [showPopup, setShowPopup] = useState(false);
+  const [menu, setMenu] = useState(false);
 
   const clickToShow = () => {
     setShowPopup(true)
@@ -16,6 +18,10 @@ function App() {
 
   const clickToClose = () => {
     setShowPopup(false)
+  }
+
+  const showMenu = () => {
+    setMenu(!menu);
   }
 
   return (
@@ -26,7 +32,11 @@ function App() {
           <Popup clickToClose={clickToClose}/> 
         </div>      
       : null}   
-      <Header />
+
+      <Header showMenu={showMenu}/>
+
+      {menu ? <BurgerMenu /> : null}   
+
       <div className="background">
       <Description />
       <Technologies clickToShow={clickToShow}/>  
